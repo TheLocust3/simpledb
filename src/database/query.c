74f9c8f01e95cd05548a470d234811398d7bcf13
TheLocust3 <jake.kinsella@gmail.com>
2020-01-23 20:13:13 -0500

    Add basic full database tests

--- src/database/query.c
+++ src/database/query.c
@@ -6,11 +6,11 @@
 
 void query_print(query_request q) {
     if (q.op == QUERY_READ) {
-        log_info("READ %d\n", q.key);
+        log_info("READ %ld\n", q.key);
     } else if (q.op == QUERY_WRITE) {
-        log_info("WRITE %d %d\n", q.key, q.val);
+        log_info("WRITE %ld %d\n", q.key, q.val);
     } else if (q.op == QUERY_REMOVE) {
-        log_info("REMOVE %d\n", q.key);
+        log_info("REMOVE %ld\n", q.key);
     } else {
         printf("Bad query operation %d received\n", q.op);
         abort();
--- test/database_test.c
+++ test/database_test.c
@@ -6,14 +6,50 @@
 
 #include "database_test.h"
 #include "../src/database/database.h"
+#include "../src/database/query.h"
 #include "helpers.h"
 
 void database_test() {
-    printf("Running full database tests\n");
+    printf("Running full database tests\n\n");
 
     database_initialize("test1.db");
 
+    printf("\n");
+
+    query_request write;
+    write.op = QUERY_WRITE;
+    write.key = 0;
+    write.val = 10;
+
+    query_request read;
+    read.op = QUERY_READ;
+    read.key = 0;
+
+    query_request delete;
+    delete.op = QUERY_REMOVE;
+    delete.key = 0;
+
+    query_response res = database_query(write);
+    assert(res.op == QUERY_WRITE);
+    assert(res.code == RESPONSE_SUCCESS);
+
+    res = database_query(read);
+    assert(res.op == QUERY_READ);
+    assert(res.code == RESPONSE_SUCCESS);
+    assert(res.val == 10);
+
+    res = database_query(delete);
+    assert(res.op == QUERY_REMOVE);
+    assert(res.code == RESPONSE_SUCCESS);
+
+    res = database_query(read);
+    assert(res.op == QUERY_READ);
+    assert(res.code == RESPONSE_SUCCESS);
+    assert(res.val == -1);
+
+    printf("\n");
+
     database_stop();
 
-    printf("Full database tests passed\n");
+    printf("\nFull database tests passed\n");
 }
